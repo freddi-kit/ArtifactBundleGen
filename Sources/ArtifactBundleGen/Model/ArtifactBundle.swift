7171a39371984248a35ff7dfa202b79844d604dc
mtj0928 <mtj0928@users.noreply.github.com>
2024-09-13 00:48:24 +0900

    Remove unnecessary code header

--- Sources/ArtifactBundleGen/Model/ArtifactBundle.swift
+++ Sources/ArtifactBundleGen/Model/ArtifactBundle.swift
@@ -1,12 +1,3 @@
-//
-//  ArtifactBundle.swift
-//
-//
-//  Created by JP29872 on 2022/12/06.
-//
-
-import Foundation
-
 struct Variant: Encodable {
     var path: String
     var supportedTriples: [String]
